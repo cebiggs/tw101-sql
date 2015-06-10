84b33c7146694f020f8b99a2814262ed64c2e17e
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-10 15:55:10 -0500

    Complete exercise 1: creating tables

---
+++ ex1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+);
+
+-- extra credit
+-- case insensitivity
+-- add extra fields
+
+create table person_downcase (
+    id integer primary key,
+    first_name text,
+    middle_name text,
+    last_name text,
+    weight float,
+    age integer
+);
