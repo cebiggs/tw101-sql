d772d684439a8388a24abf460d2a9234f22770a6
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-10 16:05:27 -0500

    Complete exercise 3: inserting data

---
+++ ex3.sql
@@ -0,0 +1,20 @@
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, "Zed", "Shaw", 37);
+	
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (1, "Cannon", "Biggs", 27);
+
+INSERT INTO pet (id, name, breed, age, dead)
+	VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet 
+	VALUES (1, "Gigantor", "Robot", 1, 1);
+
+INSERT INTO pet (id, name, breed, age, dead)
+	VALUES (2, "Kiki", "Cat", 11, 0);
+	
+INSERT INTO pet (id, name, breed, age, dead)
+	VALUES (3, "Gabby", "Cat", 3, 0);
+	
+INSERT INTO pet (id, name, breed, age, dead)
+	VALUES (4, "Babu", "Cat", 2, 0);
